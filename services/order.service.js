b3102c42a0e269f1ae4d8c969c5b7093237e41ce
Pankaj Agnihotri <pankaj.agnihotri@publicissapient.com>
2021-12-02 22:46:39 +0530

    basepath and request endpoints updated as per order service.

--- services/order.service.js
+++ services/order.service.js
@@ -4,7 +4,7 @@ import Router from 'next/router';
 import { fetchWrapper } from './../helpers/fetch-helper';
 
 const { publicRuntimeConfig } = getConfig();
-const baseUrl = `${publicRuntimeConfig.apiUrl}/order`;
+const baseUrl = `${publicRuntimeConfig.apiUrl}/order/v1`;
 
 export const orderService = {
     placeOrder,
@@ -13,23 +13,18 @@ export const orderService = {
     findAllByUserId
 };
 
-
 function findById(id) {
-    return fetchWrapper.get(`${baseUrl}/order/${id}`);
+    return fetchWrapper.get(`${baseUrl}/user/view-order?orderId=${id}`);
 }
 
 function findAllByUserId(userId) {
-    return fetchWrapper.get(`${baseUrl}/order/${userId}`);
+    return fetchWrapper.get(`${baseUrl}/user/list-orders?userId=${userId}`);
 }
 
 function placeOrder(order) {
-    return fetchWrapper.post(`${baseUrl}/order/placeOrder`);
+    return fetchWrapper.post(`${baseUrl}/user/create-order`, order);
 }
 
-function cancelOrder(id) {
-    return fetchWrapper.post(`${baseUrl}/products/${id}`);
-}
-
-function deleteProduct(id) {
-    return fetchWrapper.delete(`${baseUrl}/products/${id}`);
+function cancelOrder(order) {
+    return fetchWrapper.post(`${baseUrl}/user/cancel-order`, order);
 }
